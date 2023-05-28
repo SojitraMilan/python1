80b4cd04dc305556f04158466e6222691ca9adab
SojitraMilan <134433736+SojitraMilan@users.noreply.github.com>
2023-05-28 14:31:18 +0530

    forloop filee added

---
+++ ReversForloop.py
@@ -0,0 +1,13 @@
+
+
+num = "*"
+# reverse for loop
+for k in range(1,6,1):
+    for o in range(1,k):
+        print(num,end=" ")
+    print()
+for i in range(5, 0, -1):
+    for j in range(0, i):
+        print(num, end=' ')
+    print()
+
