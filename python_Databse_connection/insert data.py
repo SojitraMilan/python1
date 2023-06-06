3751374e4955030913b9e10fae8874941b15b3e0
SojitraMilan <134433736+SojitraMilan@users.noreply.github.com>
2023-06-06 19:16:45 +0530

    1 line added

--- python_Databse_connection/insert data.py
+++ python_Databse_connection/insert data.py
@@ -1,16 +1,16 @@
 import mysql.connector as con
 try:
-     mydb = con.connect(host = "localhost",user = "root",password='',database='python1')
+    mydb = con.connect(host = "localhost",user = "root",password='',database='python1')
   
+ 
+    mycursor = mydb.cursor()
+    id = input("Roll No:")
+    nm = input("Name:")
+    city = input("city:")
 
-     mycursor = mydb.cursor()
-     id = input("Roll No:")
-     nm = input("Name:")
-     city = input("city:")
-
-     args = (id,nm,city) 
-     mycursor.execute("insert into tb1 values(%s,%s,%s)",args)
-     print("Record inserted SuccsessFully...")
-
+    args = (id,nm,city) 
+    mycursor.execute("insert into tb1 values(%s,%s,%s)",args)
+    print("Record inserted SuccsessFully...")
+    mydb.commit()
 except:
-  print("error")
+    print("error")
